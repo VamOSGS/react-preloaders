506eb459504ac34bdd79ff81799319fa99e855ee
VamOSGS <gegham.samvelyan@hotmail.com>
2019-06-14 20:42:50 +0400

    fixed preloader closing transition

--- demo/js/Header.jsx
+++ demo/js/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => (
     <div>
       Find this package in
       <a href="https://www.npmjs.com/package/react-preloaders">
-        <Icon icon={npm} />
+        <Icon icon={npm} />PM
       </a>
       and read more in
       <a href="https://github.com/vamosgs/react-preloaders">
-        <Icon icon={github} />
+        <Icon icon={github} />Github
       </a>
     </div>
   </div>
--- src/StyledPreloader.jsx
+++ src/StyledPreloader.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+const Preloader = styled.div`
+  background: ${props => props.bgColor};
+  z-index: 9999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  transition: opacity 0.3s linear, visibility 0.2s linear 0.3s;
+  opacity: ${props => (props.loading ? 1 : 0)};
+  visibility: ${props => (props.loading ? 'visible' : 'hidden')};
+`;
 function StyledPreloader({ children, bgColor, color }) {
   const [loading, setLoading] = useState(true);
-  const Preloader = styled.div`
-    background: ${bgColor};
-    z-index: 9999;
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    transition: opacity 0.3s linear, visibility 0.2s linear 0.3s;
-    opacity: 1;
-    visibility: 1;
-  `;
   const bodyScroll = () => {
-    document.body.style.overflow = loading ? 'hidden' : 'auto';
-    document.body.style.height = loading ? '100%' : 'auto';
-    document.body.style.width = loading ? '100%' : 'auto';
-    document.body.style.position = loading ? 'fixed' : 'unset';
+    document.body.style.overflow = loading ? 'hidden' : null;
+    document.body.style.height = loading ? '100%' : null;
+    document.body.style.width = loading ? '100%' : null;
+    document.body.style.position = loading ? 'fixed' : null;
   };
   bodyScroll();
   useEffect(() => {
@@ -34,7 +34,11 @@ function StyledPreloader({ children, bgColor, color }) {
   const childrenWithProp = React.Children.map(children, child => React.cloneElement(child, {
     color,
   }));
-  return <Preloader id="preloader">{childrenWithProp}</Preloader>;
+  return (
+    <Preloader bgColor={bgColor} loading={loading} id="preloader">
+      {childrenWithProp}
+    </Preloader>
+  );
 }
 
 StyledPreloader.defaultProps = { time: 1300, bgColor: '#f7f7f7', color: '#2D2D2D' };
