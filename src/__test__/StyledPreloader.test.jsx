7f48c893ba106c5dbdc71d47f83e0cd755e0be95
VamOSGS <gegham.samvelyan@hotmail.com>
2019-06-17 01:41:49 +0400

    Test paths fix

--- src/__test__/StyledPreloader.test.jsx
+++ src/__test__/StyledPreloader.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import StyledPreloader from '../StyledPreloader';
-import Circle from '../Circle/StyledCircle';
+import StyledPreloader from '../Preloader';
+import Circle from '../Circle/Circle';
 import 'jest-styled-components';
 
 describe('StyledPreloader', () => {
